064b06b1d2d143b9c826a2d5daef30c008603116
NEXT <NEXT@NEXTui-MacBook-Pro.local>
2016-05-17 00:07:15 +0900

    [front] join modal ui 작업 완료

--- bin/static/scripts/app.js
+++ bin/static/scripts/app.js
@@ -1,10 +1,24 @@
 
 var routes = {
-  "/login" : {
-    on : function(){
-      console.log("login");
-    }
-  }
+		"/" : {
+			on : function(){
+				$(".login-modal").hide();
+				$(".join-modal").hide();
+			}
+		},
+		"/login" : {
+			on : function(){
+				console.log("login");
+				$(".login-modal").show();
+				$(".join-modal").hide();
+			}
+		},
+		"/users/form" : {
+			on : function(){
+				$(".login-modal").hide();
+				$(".join-modal").show();
+			}
+		}
 }
 
 var router = Router(routes);
