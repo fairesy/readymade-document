bb03cf8cd352403fbe56d2cd0603ffd0b6b35b6b
NEXT <NEXT@NEXTui-MacBook-Pro.local>
2016-05-26 16:15:29 +0900

    [back]bcrypt추가. 비밀번호 암호화.

--- src/main/java/com/readymade/controller/LoginController.java
+++ src/main/java/com/readymade/controller/LoginController.java
@@ -2,37 +2,40 @@ package com.readymade.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.mindrot.jbcrypt.BCrypt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.readymade.dao.UserDao;
 import com.readymade.model.User;
 
 @Controller
 public class LoginController {
+	private Logger logger = LoggerFactory.getLogger(LoginController.class);
 	@Autowired
 	UserDao userDao;
 	
 	@RequestMapping(value="/users/login", method = RequestMethod.POST)
 	public String login(@RequestParam String email, @RequestParam String password, HttpSession session){
 		
-		//id(email),pw 확인하기
 		User user = userDao.findByEmail(email);
+	
 		if(user != null){
-			if(password.equals(user.getPassword())){
-				//로그인 성공 : session에 로그인한 유저 정보 담아서 보내기 
+			if(BCrypt.checkpw(password, user.getPassword())){//hashedPassword.equals(user.getPassword());
+				logger.debug("로그인 성공");
 				session.setAttribute("user", user);
 				return "redirect:/";
 			}else{
-				//password 불일치 
+				logger.debug("패스워드 불일치");
 				return "redirect:/#/login";
 			}
 		}else{
-			//해당 이메일로 가입된 유저 없음 
+			logger.debug("해당 이메일로 가입된 유저 없음");
 			return "redirect:/#/login";
 		}
 		//TODO "패스워드가 일치하지 않습니다", "가입된 유저가 아닙니다" 등 메시지 출력
--- src/main/java/com/readymade/controller/UserController.java
+++ src/main/java/com/readymade/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.readymade.controller;
 
 
+import org.mindrot.jbcrypt.BCrypt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +15,12 @@ import com.readymade.dao.UserDao;
 import com.readymade.model.Document;
 import com.readymade.model.User;
 
+
 @Controller
 public class UserController {
+	
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	UserDao userDao;
 	
@@ -22,8 +29,11 @@ public class UserController {
 	
 	@RequestMapping(value = "/users/join", method = RequestMethod.POST)
 	public String post(@RequestParam String name, @RequestParam String email, @RequestParam String password) {
-		User user = new User(name, email, password);
 		
+		String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+		logger.debug("hashed : {}", hashedPassword);
+		User user = new User(name, email, hashedPassword);
+
 		try {
 			User joined = userDao.insert(user);
 			//문서종류가 하나이기때문에, 일단은 가입 시점에 document를 생성해서 가지고 있는다. 이후에 document 생성을 분리 
