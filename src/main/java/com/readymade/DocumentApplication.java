38ffe52e0e4e8c6e393ed729cd9dae3f656f6af3
Shinyoung <fairesy@gmail.com>
2016-05-29 01:31:50 +0900

    [back]한글 깨짐 문제 : encoding filter 추가

--- src/main/java/com/readymade/DocumentApplication.java
+++ src/main/java/com/readymade/DocumentApplication.java
@@ -2,11 +2,21 @@ package com.readymade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication(exclude={org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration.class})
 public class DocumentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DocumentApplication.class, args);
+	}
+	
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+	    final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+	    characterEncodingFilter.setEncoding("UTF-8");
+	    characterEncodingFilter.setForceEncoding(true);
+	    return characterEncodingFilter;
 	}
 }
