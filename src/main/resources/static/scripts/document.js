c98731e809ddae3bd190dbca3454f387366fe5c6
Shinyoung <fairesy@gmail.com>
2016-05-30 03:38:29 +0900

    [front]print css formatting

--- src/main/resources/static/scripts/document.js
+++ src/main/resources/static/scripts/document.js
@@ -2,10 +2,18 @@ var routes = {
 		"/documents/:documentId/new" : function(documentId){
 			InputSender.init();
 			Filter.init();
+			$(".print-button").on("click", function(){
+				
+				window.print();
+			});
 		},
 		"/documents/:documentId/edit" : function(documentId){
 			InputSender.init();
 			Filter.init();
+			$(".print-button").on("click", function(){
+				
+				window.print();
+			});
 		}
 }
 
