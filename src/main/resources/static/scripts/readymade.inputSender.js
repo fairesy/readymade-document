1dbca71b5c2da802e55ee88980cb11d343ffeacf
NEXT <NEXT@NEXTui-MacBook-Pro.local>
2016-05-30 20:14:55 +0900

    [front]filter를 클릭하여 카드를 이동할 때마다 전체 데이터 ajax 전송-DB update

--- src/main/resources/static/scripts/readymade.inputSender.js
+++ src/main/resources/static/scripts/readymade.inputSender.js
@@ -1,37 +1,34 @@
 var InputSender = (function(){
   function sendPersonalData(e){
-	  console.log("sending personal data");
 	  e.preventDefault();
+	  console.log("sending personal data");
 	  var personalInfo = $("#resume-personal-form").serialize();
-	  console.log(personalInfo);
 
 	  if(InputValidator.personalValidated()){
-		  AJAX.post("/resume/data/personal", personalInfo)
-		  .done(function(){
-		    console.log("personal 전송 완료!");
-		    $("#education").addClass("selected-card");
-		    $(".card-name .top").text("where did you");
-		    $(".card-name .bottom").text("learn?");
-		  });
+		  return AJAX.post("/resume/data/personal", personalInfo);
 	  }
-
+  }
+  
+  function toEducation(){
+	  $("#education").addClass("selected-card");
+	  $(".card-name .top").text("where did you");
+	  $(".card-name .bottom").text("learn?");
   }
 
   function sendEducationData(e){
 	  e.preventDefault();
 	  console.log("sending education data")
 	  var education = $("#resume-education-form").serialize();
-	  console.log(education);
 	  
 	  if(true){//InputValidator.educationValidated()
-		  AJAX.post("/resume/data/education", education)
-		  .done(function(){
-		    console.log("education 전송 완료!");
-		    $("#experience").addClass("selected-card");
-		    $(".card-name .top").text("any");
-		    $(".card-name .bottom").text("experiences?");
-		  });
+		  return AJAX.post("/resume/data/education", education);
 	  }
+  }
+  
+  function toExperience(){
+	  $("#experience").addClass("selected-card");
+	  $(".card-name .top").text("any");
+	  $(".card-name .bottom").text("experiences?");
   }
   
   function sendExperienceData(e){
@@ -39,14 +36,14 @@ var InputSender = (function(){
 	  console.log("sending experience data");
 	  var experience = $("#resume-experience-form").serialize();
 	  if(true){//InputValidator.experienceValidated()
-		  AJAX.post("/resume/data/experience", experience)
-		  .done(function(){
-		    console.log("experience 전송 완료!");
-		    $("#skills").addClass("selected-card");
-		    $(".card-name .top").text("and your");
-		    $(".card-name .bottom").text("skill set");
-		  });
+		  return AJAX.post("/resume/data/experience", experience);
 	  }
+  }
+  
+  function toSkills(){
+	  $("#skills").addClass("selected-card");
+	  $(".card-name .top").text("and your");
+	  $(".card-name .bottom").text("skill set");
   }
   
   function sendSkillsData(e){
@@ -55,23 +52,31 @@ var InputSender = (function(){
 	  
 	  $("#resume-skills-form input:checked").each(function(index){
 		  var skillName = $(this).val() + "-";
-		  console.log(skillName);
 		  skillList += skillName;
 	  });
-	  console.log(skillList);
 	  if(true){//InputValidator.skillsValidated()
-		  AJAX.post("/resume/data/skills", "skills="+skillList)
-		  .done(function(){
-			  console.log("skills 전송 완료!");
-			  window.print();
-		  });
+		  return AJAX.post("/resume/data/skills", "skills="+skillList);
 	  }
   }
   
+  function saveAll(e){
+	  sendPersonalData(e);
+	  sendEducationData(e);
+	  sendExperienceData(e);
+	  sendSkillsData(e);
+  }
+  
   function init(){
-	  $("#resume-personal-form input[type=submit]").on("click", sendPersonalData);
-	  $("#resume-education-form input[type=submit]").on("click", sendEducationData);
-	  $("#resume-experience-form input[type=submit]").on("click", sendExperienceData);
+	  $(".card .label").on("click", saveAll);
+	  $("#resume-personal-form input[type=submit]").on("click", function(e){
+		  sendPersonalData(e).done(toEducation);  
+	  });
+	  $("#resume-education-form input[type=submit]").on("click", function(e){
+		  sendEducationData(e).done(toExperience);  
+	  });
+	  $("#resume-experience-form input[type=submit]").on("click", function(e){
+		  sendExperienceData(e).done(toSkills);  
+	  });
 	  $("#resume-skills-form input[type=submit]").on("click", sendSkillsData);
   }
   return {
