b3be54323ab7016a2c3412595587ca6636f47421
NEXT <NEXT@NEXTui-MacBook-Pro.local>
2016-06-02 04:30:41 +0900

    [front] skill card 수정

--- src/main/java/com/readymade/DocumentApplication.java
+++ src/main/java/com/readymade/DocumentApplication.java
@@ -8,22 +8,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude={org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration.class})
+@EnableTransactionManagement
 public class DocumentApplication {
+	@Autowired
+	DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DocumentApplication.class, args);
 	}
 	
-	@Autowired
-	DataSource dataSource;
-	
 	@Bean
 	public PlatformTransactionManager txManager(){
-		DataSourceTransactionManager manager = new DataSourceTransactionManager();
-		manager.setDataSource(dataSource);
-		return manager;
+		return new DataSourceTransactionManager(dataSource);
 	}
 
 }
